39cae2523095010018f83e008187f278a48cdcc4
teslasf <bayrakta.h@gmail.com>
2025-12-28 16:08:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/index")
+@app.route("/")
 def index():
 	flash("what's your name?")
 	return render_template("index.html")
@@ -12,5 +12,6 @@ def index():
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
+
 
 
